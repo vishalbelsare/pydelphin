30bee387c708009d227a837ff714566cab703a21
Michael Wayne Goodman <goodman.m.w@gmail.com>
2018-10-27 15:38:06 -0700

    Add new dmrsjson and dmrspenman codec modules

---
+++ delphin/dmrs/dmrsjson.py
@@ -0,0 +1,184 @@
+
+"""
+DMRS-JSON serialization and deserialization.
+"""
+
+import io
+import json
+
+from delphin.sembase import Lnk, Predicate
+from delphin.dmrs import DMRS, Node, Link
+
+
+def load(source):
+    """
+    Deserialize a DMRS-JSON file (handle or filename) to DMRS objects
+
+    Args:
+        source: filename or file object
+    Returns:
+        a list of DMRS objects
+    """
+    if hasattr(source, 'read'):
+        data = json.load(source)
+    else:
+        with open(source) as fh:
+            data = json.load(fh)
+    return [from_dict(d) for d in data]
+
+
+def loads(s):
+    """
+    Deserialize a DMRS-JSON string to DMRS objects
+
+    Args:
+        s (str): a DMRS-JSON string
+    Returns:
+        a list of DMRS objects
+    """
+    data = json.loads(s)
+    return [from_dict(d) for d in data]
+
+
+def dump(ds, destination, properties=True, indent=False, encoding='utf-8'):
+    """
+    Serialize DMRS objects to a DMRS-JSON file.
+
+    Args:
+        destination: filename or file object
+        ds: iterator of :class:`~delphin.mrs.dmrs.DMRS` objects to
+            serialize
+        properties: if `True`, encode variable properties
+        indent: if `True`, adaptively indent; if `False` or `None`,
+            don't indent; if a non-negative integer N, indent N spaces
+            per level
+        encoding (str): if *destination* is a filename, write to the
+            file with the given encoding; otherwise it is ignored
+    """
+    if indent is False:
+        indent = None
+    elif indent is True:
+        indent = 2
+    data = [to_dict(d, properties=True) for d in ds]
+    if hasattr(destination, 'write'):
+        json.dump(data, destination, indent=indent)
+    else:
+        with io.open(destination, 'w', encoding=encoding) as fh:
+            json.dump(data, fh)
+
+
+def dumps(ds, properties=True, indent=False):
+    """
+    Serialize DMRS objects to a DMRS-JSON string.
+
+    Args:
+        ds: iterator of :class:`~delphin.mrs.dmrs.DMRS` objects to
+            serialize
+        properties: if `True`, encode variable properties
+        indent: if `True`, adaptively indent; if `False` or `None`,
+            don't indent; if a non-negative integer N, indent N spaces
+            per level
+    Returns:
+        a DMRS-JSON-serialization of the DMRS objects
+    """
+    if indent is False:
+        indent = None
+    elif indent is True:
+        indent = 2
+    data = [to_dict(d, properties=properties) for d in ds]
+    return json.dumps(data, indent=indent)
+
+
+def decode(s):
+    """
+    Deserialize a DMRS object from a DMRS-JSON string.
+    """
+    return from_dict(json.loads(s))
+
+
+def encode(d, properties=True, indent=False):
+    """
+    Serialize a DMRS object to a DMRS-JSON string.
+
+    Args:
+        d: a DMRS object
+        properties (bool): if `False`, suppress variable properties
+        indent (bool, int): if `True` or an integer value, add
+            newlines and indentation
+    Returns:
+        a DMRS-JSON-serialization of the DMRS object
+    """
+    if indent is False:
+        indent = None
+    elif indent is True:
+        indent = 2
+    return json.dumps(to_dict(d, properties=properties), indent=indent)
+
+
+def to_dict(dmrs, short_pred=True, properties=True):
+    """
+    Encode *dmrs* as a dictionary suitable for JSON serialization.
+    """
+    getpred = Predicate.short_form if short_pred else Predicate.string
+    nodes=[]
+    for node in dmrs.nodes:
+        n = dict(nodeid=node.nodeid,
+                 predicate=getpred(node.predicate))
+        if node.lnk is not None:
+            n['lnk'] = {'from': node.cfrom, 'to': node.cto}
+        if properties and node.sortinfo:
+            n['sortinfo'] = node.sortinfo
+        if node.surface is not None:
+            n['surface'] = node.surface
+        if node.base is not None:
+            n['base'] = node.base
+        if node.carg is not None:
+            n['carg'] = node.carg
+        nodes.append(n)
+    links=[]
+    for link in dmrs.links:
+        links.append({
+            'from': link.start, 'to': link.end,
+            'rargname': link.rargname, 'post': link.post
+        })
+    d = dict(nodes=nodes, links=links)
+    for attr in ('top', 'index', 'xarg', 'lnk', 'surface', 'identifier'):
+        val = getattr(dmrs, attr, None)
+        if val is not None:
+            d[attr] = val
+    return d
+
+
+def from_dict(d):
+    """
+    Decode a dictionary, as from :func:`to_dict`, into a DMRS object.
+    """
+    def _lnk(x):
+        return None if x is None else Lnk.charspan(x['from'], x['to'])
+    nodes = []
+    for node in d.get('nodes', []):
+        nodes.append(Node(
+            node['nodeid'],
+            node['predicate'],
+            sortinfo=node.get('sortinfo'),
+            lnk=_lnk(node.get('lnk')),
+            surface=node.get('surface'),
+            base=node.get('base'),
+            carg=node.get('carg')))
+    links = []
+    for link in d.get('links', []):
+        links.append(Link(
+            link['from'],
+            link['to'],
+            link.get('rargname'),
+            link.get('post')))
+    return DMRS(
+        top=d.get('top'),
+        index=d.get('index'),
+        xarg=d.get('xarg'),
+        nodes=nodes,
+        links=links,
+        lnk=_lnk(d.get('lnk')),
+        surface=d.get('surface'),
+        identifier=d.get('identifier')
+    )
---
+++ delphin/dmrs/dmrspenman.py
@@ -0,0 +1,186 @@
+
+"""
+DMRS-PENMAN serialization and deserialization.
+"""
+
+import io
+
+import penman
+
+from delphin.sembase import Lnk, Predicate
+from delphin.dmrs import DMRS, Node, Link
+from delphin.dmrs._dmrs import FIRST_NODEID
+
+
+def load(source):
+    """
+    Deserialize PENMAN graphs from a file (handle or filename)
+
+    Args:
+        source: filename or file object
+    Returns:
+        a list of DMRS objects
+    """
+    graphs = penman.load(fh)
+    xs = [from_triples(g.triples()) for g in graphs]
+    return xs
+
+
+def loads(s):
+    """
+    Deserialize PENMAN graphs from a string
+
+    Args:
+        s (str): serialized PENMAN graphs
+    Returns:
+        a list of DMRS objects
+    """
+    graphs = penman.loads(s)
+    xs = [from_triples(g.triples()) for g in graphs]
+    return xs
+
+
+def dump(ds, destination, properties=False, indent=True, encoding='utf-8'):
+    """
+    Serialize DMRS objects to a PENMAN file.
+
+    Args:
+        destination: filename or file object
+        ds: iterator of :class:`~delphin.mrs.dmrs.DMRS` objects to
+            serialize
+        properties: if `True`, encode variable properties
+        indent: if `True`, adaptively indent; if `False` or `None`,
+            don't indent; if a non-negative integer N, indent N spaces
+            per level
+        encoding (str): if *destination* is a filename, write to the
+            file with the given encoding; otherwise it is ignored
+    """
+    text = dumps(xs, properties=properties, indent=indent)
+    if hasattr(destination, 'write'):
+        print(text, file=destination)
+    else:
+        with io.open(destination, 'w', encoding=encoding) as fh:
+            print(text, file=fh)
+
+
+def dumps(ds, properties=False, indent=True):
+    """
+    Serialize DMRS objects to a PENMAN string.
+
+    Args:
+        ds: iterator of :class:`~delphin.mrs.dmrs.DMRS` objects to
+            serialize
+        properties: if `True`, encode variable properties
+        indent: if `True`, adaptively indent; if `False` or `None`,
+            don't indent; if a non-negative integer N, indent N spaces
+            per level
+    Returns:
+        a PENMAN-serialization of the DMRS objects
+    """
+    codec = penman.PENMANCodec()
+    to_graph = codec.triples_to_graph
+    graphs = [to_graph(to_triples(d, properties=properties)) for d in ds]
+    return penman.dumps(graphs, indent=indent)
+
+
+def decode(s):
+    """
+    Deserialize a DMRS object from a PENMAN string.
+    """
+    return from_triples(penman.decode(s).triples())
+
+
+def encode(d, properties=True, indent=False):
+    """
+    Serialize a DMRS object to a PENMAN string.
+
+    Args:
+        d: a DMRS object
+        properties (bool): if `False`, suppress variable properties
+        indent (bool, int): if `True` or an integer value, add
+            newlines and indentation
+    Returns:
+        a PENMAN-serialization of the DMRS object
+    """
+    triples = to_triples(d, properties=properties)
+    g = penman.PENMANCodec().triples_to_graph(triples)
+    return penman.encode(g, indent=indent)
+
+
+def to_triples(dmrs, short_pred=True, properties=True):
+    """
+    Encode *dmrs* as triples suitable for PENMAN serialization.
+    """
+    getpred = Predicate.short_form if short_pred else Predicate.string
+    ts = []
+    for node in dmrs.nodes:
+        ts.append((node.nodeid, 'instance', getpred(node.predicate)))
+        if node.lnk is not None:
+            ts.append((node.nodeid, 'lnk', '"{}"'.format(str(node.lnk))))
+        if node.carg is not None:
+            ts.append((node.nodeid, 'carg', '"{}"'.format(node.carg)))
+        if properties:
+            for key, value in node.sortinfo.items():
+                ts.append((node.nodeid, key.lower(), value))
+
+    if dmrs.top is not None:
+        ts.append((None, 'top', dmrs.top))
+    for link in dmrs.links:
+        relation = '{}-{}'.format(link.role.upper(), link.post)
+        ts.append((link.start, relation, link.end))
+    return ts
+
+
+def from_triples(triples, remap_nodeids=True):
+    """
+    Decode triples into a DMRS object.
+    """
+    top = lnk = surface = identifier = None
+    nids, nd, edges = [], {}, []
+    for src, rel, tgt in triples:
+        src, tgt = str(src), str(tgt)  # hack for int-converted src/tgt
+        if src is None and rel == 'top':
+            top = tgt
+            continue
+        elif src not in nd:
+            if top is None:
+                top=src
+            nids.append(src)
+            nd[src] = {'pred': None, 'lnk': None, 'carg': None, 'si': []}
+        if rel == 'instance':
+            nd[src]['pred'] = Predicate.surface_or_abstract(tgt)
+        elif rel == 'lnk':
+            cfrom, cto = tgt.strip('"<>').split(':')
+            nd[src]['lnk'] = Lnk.charspan(int(cfrom), int(cto))
+        elif rel == 'carg':
+            if (tgt[0], tgt[-1]) == ('"', '"'):
+                tgt = tgt[1:-1]
+            nd[src]['carg'] = tgt
+        elif rel.islower():
+            nd[src]['si'].append((rel, tgt))
+        else:
+            rargname, post = rel.rsplit('-', 1)
+            edges.append((src, tgt, rargname, post))
+    if remap_nodeids:
+        nidmap = dict((nid, FIRST_NODEID+i) for i, nid in enumerate(nids))
+    else:
+        nidmap = dict((nid, nid) for nid in nids)
+    nodes = [
+        Node(
+            nodeid=nidmap[nid],
+            predicate=nd[nid]['pred'],
+            sortinfo=nd[nid]['si'],
+            lnk=nd[nid]['lnk'],
+            carg=nd[nid]['carg']
+        ) for i, nid in enumerate(nids)
+    ]
+    links = [Link(nidmap[s], nidmap[t], r, p) for s, t, r, p in edges]
+    return DMRS(
+        top=nidmap[top],
+        nodes=nodes,
+        links=links,
+        lnk=lnk,
+        surface=surface,
+        identifier=identifier
+    )
+
